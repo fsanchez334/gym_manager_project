23c490cac5112423efb2769b6c6022642e714f29
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-06-18 21:12:58 -0400

    So far, I've made the structs for a gym equipment and a person using the equipment. I also made a struct for a timer that keeps tracks of how long the person is on an equipment

--- gym.c
+++ gym.c
@@ -1,5 +1,27 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+struct Timer{
+    int hours = 0;
+    int minute = 0;
+    int second = 0;
+};
+struct Treadmill{
+    char status[21];
+    int time;
+    char user_id[41];
+    char mode;
+};
+
+struct Person{
+    char first_name[51];
+    char last_name[51];
+};
+int main(){
+    printf("%s\n", "We are going to simulate a gym");
+    return 0;
+
+}
 
