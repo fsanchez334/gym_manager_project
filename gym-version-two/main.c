78133dc16745bbcb2ecfb57e2627f1338417aca9
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-06-22 00:26:57 -0400

    Added code that asks the user the number of people that will be using the treadmill. Subsequently, the code makes new Person and Id objects for each iteration

--- gym-version-two/main.c
+++ gym-version-two/main.c
@@ -6,17 +6,64 @@
 
 struct ID{
     char *name;
-    char *age;
+    char *class
+};
 
+struct Person{
+    struct ID *personal;
+};
+
+struct Treadmill{
+    char *status;
+    struct ID *holder;
 };
 
 int main(){
+
     printf("%s\n", "Hello, welcome to the gym! For now there are only treadmills available");
-    printf("%s", "How many people are going to use the gym?");
+    printf("%s\n", "How many people are going to use the gym?");
 
     int amount_people;
-    scanf("%d", &amount_of_people);
+    scanf("%d", &amount_people);
 
-    printf("%d\n", amount_of_people);
+    printf("%d\n", amount_people);
+
+    struct Person container[amount_people];
+    struct ID box[amount_people];
+
+    int racker = 0;
+    
+
+    for(int i = 0; i < amount_people; i++){
+         char f_name[50];
+	 char l_name[50];
+	 char sum[100];
+	 char class_00[40];
+
+	 printf("%d%s\n", tracker, "What is your first name?");
+	 scanf("%s", f_name);
+
+	 printf("%s\n", "What is your last name?");
+	 scanf("%s", l_name);
+
+	 printf("%s\n", "What is your class year? Freshman, Sophomore, Junior, Senior");
+	 scanf("%s", class_00);
+
+	 strcpy(sum, f_name);
+	 strcat(sum, " ");
+	 strcat(sum, l_name);
+
+	 box[i].name = sum;
+	 box[i].class = class_00;
+
+	 container[i].personal = box[i];
+
+	 ++tracker;
+
+    }
+
+    printf("%s\n", "Success");
+
+    
 }
 
